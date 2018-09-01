9d1a9a38a803a9797d9e494cff664da810c8d198
Artem Shumilov <vanbellum@gmail.com>
2018-09-01 18:47:52 +0300

    re factored recyclerview adapters

--- app/src/main/java/ru/supernacho/tkb/tz/moneytransfer/view/adapter/BeneficiaryRvAdapter.java
+++ app/src/main/java/ru/supernacho/tkb/tz/moneytransfer/view/adapter/BeneficiaryRvAdapter.java
@@ -1,69 +1,26 @@
 package ru.supernacho.tkb.tz.moneytransfer.view.adapter;
 
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import java.util.List;
-
-import ru.supernacho.tkb.tz.moneytransfer.R;
 import ru.supernacho.tkb.tz.moneytransfer.model.entity.Card;
 import ru.supernacho.tkb.tz.moneytransfer.presenter.MainActivityPresenter;
 import ru.supernacho.tkb.tz.moneytransfer.view.CachedCardView;
 import ru.supernacho.tkb.tz.moneytransfer.view.NewCardView;
 
-public class BeneficiaryRvAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private MainActivityPresenter presenter;
-    private List<Card> cards;
+public class BeneficiaryRvAdapter extends AbstractRvAdapter {
 
     public BeneficiaryRvAdapter(MainActivityPresenter presenter) {
-        this.presenter = presenter;
-        this.cards = presenter.getBeneficiaryCards();
+        super(presenter, true);
     }
 
     @Override
-    public int getItemViewType(int position) {
-        if (cards.get(position).isNewCard()) return ViewType.NEW_CARD;
-        else return ViewType.CACHED_CARD;
-    }
-
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view;
-        switch (viewType) {
-            case ViewType.NEW_CARD:
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.new_card_view_holder, parent, false);
-                return new NewCardView(view, presenter, true);
-            default:
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cached_card_view, parent, false);
-                return new CachedCardView(view, presenter, true);
-        }
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        int viewType = holder.getItemViewType();
-        Card card = cards.get(position);
-        switch (viewType) {
-            case ViewType.CACHED_CARD:
-                prepareCachedCard((CachedCardView) holder, card);
-                break;
-            default:
-                prepareNewCardData((NewCardView) holder, card);
-                break;
-        }
-    }
-
-    private void prepareNewCardData(@NonNull NewCardView holder, Card card) {
+    void prepareNewCardData(@NonNull NewCardView holder, Card card) {
         holder.setCard(card);
         holder.etCardNumber.setText(null);
         holder.etCardNumber.clearFocus();
     }
 
-    private void prepareCachedCard(@NonNull CachedCardView holder, Card card) {
+    @Override
+    void prepareCachedCardData(@NonNull CachedCardView holder, Card card) {
         holder.setCard(card);
         holder.tvCardNumber.setText(card.getNumber());
         holder.tvBanklabel.setText(card.getBankName());
--- app/src/main/java/ru/supernacho/tkb/tz/moneytransfer/view/adapter/SenderRvAdapter.java
+++ app/src/main/java/ru/supernacho/tkb/tz/moneytransfer/view/adapter/SenderRvAdapter.java
@@ -1,63 +1,19 @@
 package ru.supernacho.tkb.tz.moneytransfer.view.adapter;
 
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import java.util.List;
-
-import ru.supernacho.tkb.tz.moneytransfer.R;
 import ru.supernacho.tkb.tz.moneytransfer.model.entity.Card;
 import ru.supernacho.tkb.tz.moneytransfer.presenter.MainActivityPresenter;
 import ru.supernacho.tkb.tz.moneytransfer.view.CachedCardView;
 import ru.supernacho.tkb.tz.moneytransfer.view.NewCardView;
 
-public class SenderRvAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private MainActivityPresenter presenter;
-    private List<Card> cards;
+public class SenderRvAdapter extends AbstractRvAdapter {
 
     public SenderRvAdapter(MainActivityPresenter presenter) {
-        this.presenter = presenter;
-        this.cards = presenter.getSenderCards();
+        super(presenter, false);
     }
 
     @Override
-    public int getItemViewType(int position) {
-        if (cards.get(position).isNewCard()) return ViewType.NEW_CARD;
-        else return ViewType.CACHED_CARD;
-    }
-
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view;
-        switch (viewType) {
-            case ViewType.NEW_CARD:
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.new_card_view_holder, parent, false);
-                return new NewCardView(view, presenter,false);
-            default:
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cached_card_view, parent, false);
-                return new CachedCardView(view, presenter,false);
-        }
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        int viewType = holder.getItemViewType();
-        Card card = cards.get(position);
-        switch (viewType) {
-            case ViewType.CACHED_CARD:
-                prepareCachedCardData((CachedCardView) holder, card);
-                break;
-            default:
-                prepareNewCardData((NewCardView) holder, card);
-                break;
-        }
-    }
-
-    private void prepareCachedCardData(@NonNull CachedCardView holder, Card card) {
+    void prepareCachedCardData(@NonNull CachedCardView holder, Card card) {
         holder.setCard(card);
         holder.tvCardNumber.setText(card.getNumber());
         holder.tvBanklabel.setText(card.getBankName());
@@ -65,7 +21,8 @@ public class SenderRvAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         holder.etCacheCVV.clearFocus();
     }
 
-    private void prepareNewCardData(@NonNull NewCardView holder, Card card) {
+    @Override
+    void prepareNewCardData(@NonNull NewCardView holder, Card card) {
         holder.setCard(card);
         holder.etCardNumber.setText(null);
         holder.etCardNumber.clearFocus();
